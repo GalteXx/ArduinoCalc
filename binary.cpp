8b569a335144bdc9fae2ee2a33ac863805ff8b80
TaggedDev <45800215+TaggedDev@users.noreply.github.com>
2020-09-16 11:37:36 +0300

    Console v

---
+++ binary.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int source = 0;
+	cin >> source;
+    int t = 0, d = 1;
+    while (source > 0)
+    {
+        t += (source % 2) * d;
+        source = source / 2;
+        d = d * 10; // razryad
+    }
+    cout << t;
+	return 0;
+}
