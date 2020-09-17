755c744acf972b6cdcb342372bcba3640ecfa600
GalteXx <65533265+GalteXx@users.noreply.github.com>
2020-09-17 10:03:18 +0300

    Add files via upload

--- sketch_sep16b/sketch_sep16b.ino
+++ sketch_sep16b/sketch_sep16b.ino
@@ -1,5 +1,18 @@
+long binary(long source)
+{ long t = 0, d = 1;
+      while (source > 0)
+    {
+        t += (source % 2) * d;
+        source = source / 2;
+        d = d * 10; // razryad
+    }
+    return t;
+}
+
 void setup() {
     int a = 3;
+    //3 5 6 9 10 11
+    pinMode(13, OUTPUT);
     while(a < 12)
     {
       pinMode(a, OUTPUT);
@@ -9,24 +22,31 @@ void setup() {
 }
 
 void loop() {
-      long t = 0, d = 1;
-digitalWrite(2, HIGH);
-  
+  bool flag = true;
+//digitalWrite(2, HIGH);
+  long prihod = 0, t;
   int a = 3;
   long source = 0;
-  if (Serial.available() > 0)
+  while (flag)
   {
-  source = Serial.readString().toInt();
-  
-     Serial.print(source);
-    while (source > 0)
-    {
-        t += (source % 2) * d;
-        source = source / 2;
-        d = d * 10; // razryad
-    }
-    Serial.print(t);
+    
+  source = Serial.read();
+  if( source > 47 && source <= 57)
+  {
+  prihod = (prihod * 10) + (source - 48);
+  source = 0;
   }
+  else flag = false;
+  }
+  Serial.println(prihod);
+  delay (1000);
+  if(prihod >= 1024)
+  {
+    tone(13, 600);
+  }
+
+  
+    t = binary(prihod);  
     while(t > 0)
     {
       if(t % 10 == 1)
